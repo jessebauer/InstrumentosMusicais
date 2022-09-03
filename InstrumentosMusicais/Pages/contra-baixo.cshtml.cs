b6df6764a925a77f310fc4e79c8bcdf46724e737
Jesse <jesse_ruan@hotmail.com>
2022-09-02 22:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ InstrumentosMusicais/Pages/contra-baixo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InstrumentosMusicais.Pages
+{
+    public class contra_baixoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ InstrumentosMusicais/Pages/violao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InstrumentosMusicais.Pages
+{
+    public class violaoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
